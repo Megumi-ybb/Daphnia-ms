e3bdee1a6c0e79201f56b1f36a552c4284d58e52
BO YANG <ybb@umich.edu>
2025-05-14 15:05:45 -0400

    update of profile

---
+++ Mixed-species/SRJF2/profile_previous/rn/profile_rn.R
@@ -0,0 +1,419 @@
+library(reshape2)
+library(magrittr)
+library(foreach)
+library(readxl)
+library(doParallel)
+registerDoParallel(cores=36)
+library(pomp)
+library(panelPomp)
+library(tidyverse)
+
+# Mesocosm_data = read_excel("/Users/ybb/Desktop/Research//Daphnia/Mesocosmdata.xls",3)
+Mesocosm_data = read_excel("/home/ybb/D_P/Mesocosmdata.xlsx",3)
+
+
+sed = sample(1:1000000,1)
+set.seed(sed)
+
+name_str = "rn"
+run_level <- 2
+
+dentNoPara <- Mesocosm_data[1:90, ]
+dentNoPara <- subset(dentNoPara, select = c(rep, day, dent.adult,lum.adult))
+dentNoPara <- dentNoPara[90: 1, ]
+#Convert sampling date to natural date. Samples are collected every 5 days after the #first 7 days.
+dentNoPara$day = (dentNoPara$day - 1) * 5 + 7
+data = list()
+trails = c("A","C","D","E","F","G","H","I","J")
+for (i in 1: length(trails)){
+  data[[i]]=subset(dentNoPara, select = c("day", "dent.adult","lum.adult"),
+                   dentNoPara$rep == trails[i])
+}
+
+
+dyn_rpro <- Csnippet("
+                      double Sn_term, F_term , Si_term,Jn_term,Ji_term;
+                      double noiSn, noiSi  ,noiF,noiJn,noiJi;
+                      double delta = 0.013; //fraction of volume replaced day-1
+
+                      noiSn = rnorm(0, sigSn * sqrt(dt));
+                      noiSi = rnorm(0, sigSi * sqrt(dt));
+                      noiJn = rnorm(0, sigJn * sqrt(dt));
+                      noiJi = rnorm(0, sigJi * sqrt(dt));
+                      noiF = rnorm(0, sigF * sqrt(dt));
+
+
+                      //------------Sn-------------
+                      Sn_term = 0.1 * Jn * dt - theta_Sn * Sn * dt -  delta * Sn * dt + Sn * noiSn;
+                        
+                      //-----------Jn-------------
+                      
+                      Jn_term = rn * f_Sn * F * Sn  * dt  -  0.1 * Jn * dt - theta_Jn * Jn * dt - delta * Jn * dt + Jn * noiJn;
+                      
+
+                      //------------Si-------------
+                      Si_term = 0.1 * Ji * dt - theta_Si * Si * dt - delta * Si * dt + Si * noiSi;
+
+                      //-----------Ji-------------
+                      
+                      Ji_term = ri * f_Si *F * Si  * dt - 0.1 * Ji * dt - theta_Ji * Ji * dt - delta * Ji * dt + Ji * noiJi;
+                      
+                      //-----------F---------------
+
+                      F_term =  F * noiF - f_Sn * F * (Sn + 1 * Jn) * dt - f_Si * F * (Si + 1 * Ji) * dt - delta * F * dt + 0.37 * dt;
+
+
+                      //T_D and T_I are the S and I Daphnia sample out
+                      F += F_term;
+                      Sn += Sn_term;
+                      Si += Si_term;
+                      Ji += Ji_term;
+                      Jn += Jn_term;
+                  
+                      
+                      
+                      if (Sn < 0.0 || Sn > 1e5) {
+                        Sn = 0.0;
+                        error_count += 1;
+                      }
+                      if (Si < 0.0 || Si > 1e5) {
+                        Si = 0.0;
+                        error_count += 1000000;
+                      }
+                      if (F < 0.0 || F > 1e20) {
+                        F = 0.0;
+                        error_count += 1000;
+                      }
+                      if (Jn < 0.0 || Jn > 1e5) {
+                        Jn = 0.0;
+                        error_count += 0.001;
+                      }
+                      if (Ji < 0.0 || Ji > 1e5) {
+                        Ji = 0.0;
+                        error_count += 0.000000001;
+                      }   
+                      
+                      T_Sn = fabs(Sn);
+                      T_Si = fabs(Si);
+                      
+
+                      ")
+
+# Initial state. Assume t0 = day 4
+dyn_init = Csnippet("
+                     Sn = 2.333; //2.3333 = 35/15L
+                     Si = 0.667; //0.667 = 10/15L
+                     F = 16.667;
+                     Jn = 0;
+                     Ji = 0;
+                     
+                     T_Sn = 0.0;
+                     T_Si = 0.0;
+                     
+                     error_count = 0.0;
+                     
+                     ")
+
+
+
+dmeas = Csnippet("
+                 
+                 if (error_count > 0.0) {
+                   lik = -150;
+                  }
+                 else{
+                    if(give_log){
+                    lik  = dnbinom_mu(lumadult,k_Si,T_Si,give_log)  + dnbinom_mu(dentadult,k_Sn,T_Sn,give_log);
+                    }
+                    else{
+                    lik  = dnbinom_mu(lumadult,k_Si,T_Si,give_log) * dnbinom_mu(dentadult,k_Sn,T_Sn,give_log);
+                    }
+                      }
+                 ")
+
+rmeas = Csnippet("
+                 //double delta = 0.013;
+                 dentadult = rnbinom_mu(k_Sn,T_Sn);
+                 lumadult = rnbinom_mu(k_Si,T_Si);
+                 ")
+
+pt <- parameter_trans(
+  log = c( "sigSn", "sigSi","sigF","f_Sn","f_Si",
+           "rn","ri","k_Sn","k_Si","sigJi","sigJn",
+           "theta_Sn","theta_Si","theta_Jn","theta_Ji")
+)
+
+pomplist=list()
+
+parameters=  c( "sigSn" = 0.1, "sigSi"= 0.1,"sigF"= 0.1,"f_Sn"= 0.1,"f_Si"= 0.1,
+                "rn"= 10,"ri"= 10,"k_Sn"= 0.1,"k_Si"= 0.1,"sigJi"= 0.1,"sigJn"= 0.1,
+                "theta_Sn"= 0.1,"theta_Si"= 0.1,"theta_Jn"= 0.1,"theta_Ji"= 0.1)
+
+
+for (i in 1:9){
+  colnames(data[[i]]) <- c('day', 'dentadult', 'lumadult')
+  pomp(data = data[[i]],
+       times = "day",
+       t0=1,
+       rprocess=euler(dyn_rpro,delta.t=1/4),
+       rinit = dyn_init,
+       dmeasure = dmeas,
+       rmeasure = rmeas,
+       partrans = pt,
+       obsnames = c("dentadult","lumadult"),
+       accumvars = c("error_count"),
+       paramnames = c( "sigSn", "sigSi","sigF","f_Sn","f_Si",
+                       "rn","ri","k_Sn","k_Si","sigJi","sigJn",
+                       "theta_Sn","theta_Si","theta_Jn","theta_Ji"),
+       statenames = c("Sn", "Si","Jn","Ji" ,"error_count", "F", "T_Sn","T_Si")
+  ) -> pomplist[[i]]
+  coef(pomplist[[i]])=parameters
+}
+names(pomplist)=paste("u", 1:9,sep = "")
+
+
+
+shared_parameter = c(
+  ri = 100.72, rn = 100.94, f_Si = 5.178821e-05, f_Sn = 9.394652e-05, 
+  theta_Sn = 0.5026099, theta_Si = 0.6418282, theta_Ji = 0.8648736, theta_Jn = 0.9614655, 
+  sigSn = 0, sigSi = 0, sigJi = 0.12, sigJn = 0.1879358, 
+  sigF = 0.05304384, k_Si = 2.665681, k_Sn = 14.89269
+)
+
+
+panelfood = panelPomp(pomplist, shared=shared_parameter)
+
+
+
+shared_parameter <- c(
+  ri        = 5.487952e+03,
+  rn        = 1.052436e+04,
+  f_Si      = 1.209614e-04,
+  f_Sn      = 4.772315e-05,
+  theta_Sn  = 1.254196e+00,
+  theta_Si  = 4.203929e-01,
+  theta_Ji  = 1.418756e+00,
+  theta_Jn  = 1.892235e-01,
+  sigSn     = 0.000000e+00,
+  sigSi     = 0.000000e+00,
+  sigJi     = 1.124274e-02,
+  sigJn     = 3.991407e-01,
+  sigF      = 4.334792e-02,
+  k_Si      = 2.216602e+00,
+  k_Sn      = 1.618056e+02
+)
+
+
+generate_parameter_profile <- function(prof_name, nprof = 80) {
+  shared_ub <- shared_parameter * 10
+  
+  shared_lb <- shared_ub / 100
+  
+  ub_unit <- shared_ub[prof_name]
+  lb_unit <- shared_lb[prof_name]
+  
+  prof_value <- seq(lb_unit, ub_unit, length.out = nprof)
+  
+  prof_cols <- matrix(rep(prof_value, 35), ncol = 1)
+  prof_cols <- as.matrix(sort(prof_cols))
+  colnames(prof_cols) <- prof_name
+  
+  shared_ub <- shared_ub[!names(shared_ub) %in% prof_name]
+  shared_lb <- shared_lb[!names(shared_lb) %in% prof_name]
+  
+  guesses_shared <- runif_design(
+    lower = shared_lb,
+    upper = shared_ub,
+    nseq = nprof * 35
+  )
+  
+  parameter_shared <- cbind(prof_cols, guesses_shared)
+  
+  return(parameter_shared)
+}
+
+generate_sd <- function(x = 0.05, profile_name){
+  sd_list = c(
+    ri        = x,
+    rn        = x,
+    f_Si      = x,
+    f_Sn      = x,
+    theta_Sn  = x,
+    theta_Si  = x,
+    theta_Ji  = x,
+    theta_Jn  = x,
+    sigSn     = 0,
+    sigSi     = 0,
+    sigJi     = x,
+    sigJn     = x,
+    sigF      = x,
+    k_Si      = x,
+    k_Sn      = x
+  )
+  
+  sd_list[profile_name] = 0
+  return(sd_list)
+}
+
+parameter_shared <- generate_parameter_profile(name_str)
+
+algorithmic.params <- list(
+  Np =     c(50, 320, 1e4),
+  Np_rep = c( 2,  10,  10),
+  Mp =     c(50, 400, 1e4),
+  Nmif =   c( 2,  320, 250)
+)
+
+
+dent_rw_sd_first = generate_sd(x = 0.05,profile_name = name_str)
+
+U = length(panelfood)
+{
+  foreach(
+    i = 1:nrow(parameter_shared),
+    .packages = c("pomp", "panelPomp"),
+    .inorder = FALSE,
+    .options.multicore = list(set.seed = TRUE)
+  ) %dopar%
+    {
+      guessed.parameter.values <- as.numeric(parameter_shared[i,])
+      names(guessed.parameter.values) = colnames(parameter_shared)
+      mif2(
+        panelfood,
+        Nmif = 150,
+        shared.start = guessed.parameter.values,
+        rw.sd = rw_sd(
+          sigSn=dent_rw_sd_first['sigSn'],
+          sigSi=dent_rw_sd_first['sigSi'],
+          sigF=dent_rw_sd_first['sigF'],
+          theta_Sn=dent_rw_sd_first['theta_Sn'],
+          theta_Si=dent_rw_sd_first['theta_Si'],
+          k_Sn = dent_rw_sd_first['k_Sn'],
+          k_Si = dent_rw_sd_first['k_Si'],
+          f_Sn=dent_rw_sd_first['f_Sn'],
+          f_Si=dent_rw_sd_first['f_Si'],
+          rn=dent_rw_sd_first['rn'],
+          ri=dent_rw_sd_first['ri'],
+          sigJi = dent_rw_sd_first['sigJi'],
+          sigJn = dent_rw_sd_first['sigJn'],
+          theta_Jn = dent_rw_sd_first['theta_Jn'],
+          theta_Ji = dent_rw_sd_first['theta_Ji']),
+        cooling.type = "geometric",
+        cooling.fraction.50 = 0.7,
+        Np = algorithmic.params$Mp[run_level]
+        
+      ) -> m1
+      
+      ll <- replicate(n = algorithmic.params$Np_rep[run_level],
+                      unitlogLik(pfilter(m1,
+                                         Np = algorithmic.params$Np[run_level])))
+      
+      list(mif = m1,
+           ll = panel_logmeanexp(x = ll,
+                                 MARGIN = 1,
+                                 se = TRUE))
+    }
+} -> mf1
+
+log_list = c()
+for ( i in 1:length(mf1)){
+  log_list = c(log_list,mf1[[i]]$ll[1])
+}
+select = order(log_list,decreasing = TRUE)[1:ceiling(length(mf1)/4)]
+shared_dataframe = data.frame(t(mf1[[select[1]]]$mif@shared))
+for (i in 1:length(select)){
+  shared_dataframe[i,] = t(mf1[[select[i]]]$mif@shared)
+}
+shared_dataframe <- shared_dataframe[rep(1:nrow(shared_dataframe), each = 4), ]
+dent_rw_sd_second = generate_sd(x = 0.04,profile_name = name_str)
+#round 2
+{
+  foreach(
+    i = 1:nrow(parameter_shared),
+    .packages = c("pomp", "panelPomp"),
+    .inorder = FALSE,
+    .options.multicore = list(set.seed = TRUE)
+  ) %dopar%
+    {
+      share_para_temp = as.numeric(shared_dataframe[i,])
+      names(share_para_temp) = colnames(shared_dataframe)
+      
+      mif2(
+        panelfood,
+        Nmif = 300,
+        shared.start = share_para_temp,
+        rw.sd = rw_sd(
+          sigSn=dent_rw_sd_first['sigSn'],
+          sigSi=dent_rw_sd_first['sigSi'],
+          sigF=dent_rw_sd_first['sigF'],
+          theta_Sn=dent_rw_sd_first['theta_Sn'],
+          theta_Si=dent_rw_sd_first['theta_Si'],
+          k_Sn = dent_rw_sd_first['k_Sn'],
+          k_Si = dent_rw_sd_first['k_Si'],
+          f_Sn=dent_rw_sd_first['f_Sn'],
+          f_Si=dent_rw_sd_first['f_Si'],
+          rn=dent_rw_sd_first['rn'],
+          ri=dent_rw_sd_first['ri'],
+          sigJi = dent_rw_sd_first['sigJi'],
+          sigJn = dent_rw_sd_first['sigJn'],
+          theta_Jn = dent_rw_sd_first['theta_Jn'],
+          theta_Ji = dent_rw_sd_first['theta_Ji']),
+        cooling.type = "geometric",
+        cooling.fraction.50 = 0.7,
+        Np = algorithmic.params$Mp[run_level]
+      ) -> m1
+      
+      ll <- replicate(n = algorithmic.params$Np_rep[run_level],
+                      unitlogLik(pfilter(m1,
+                                         Np = algorithmic.params$Np[run_level])))
+      
+      list(mif = m1,
+           ll = panel_logmeanexp(x = ll,
+                                 MARGIN = 1,
+                                 se = TRUE))
+    }
+} -> mf
+
+
+
+lls <- matrix(unlist(sapply(mf, getElement, "ll")), nrow = 2)
+best <- which.max(lls[1,])
+mif.estimate <- coef(mf[[best]]$mif)
+pf.loglik.of.mif.estimate <- unname(mf[[best]]$ll[1])
+s.e.of.pf.loglik.of.mif.estimate <- unname(mf[[best]]$ll[2])
+
+trace <- as.data.frame(traces(mf[[1]]$mif))
+trace$iter = as.numeric(rownames(trace))
+
+for (i in 2 : length(mf)){
+  tmp_df <- as.data.frame(traces(mf[[i]]$mif))
+  tmp_df$iter = as.numeric(rownames(tmp_df))
+  trace <- dplyr::bind_rows(trace,tmp_df)
+}
+
+final_likes <- numeric(length(mf))
+
+for (i in 1: length(mf)){
+  final_likes[i] <- mf[[i]]$ll[1]
+}
+
+final_params <- trace %>%
+  dplyr::filter(iter == max(iter, na.rm = TRUE))
+
+final_params$loglik <- final_likes
+
+pf.loglik.of.mif.estimate
+s.e.of.pf.loglik.of.mif.estimate
+
+if (run_level == 2){
+  save(mf,final_params,lls,best,mif.estimate,pf.loglik.of.mif.estimate,
+       s.e.of.pf.loglik.of.mif.estimate,
+       file = paste0(name_str,".RData"))
+}
+
+if (run_level == 3){
+  save(lls,best,mif.estimate,pf.loglik.of.mif.estimate,
+       s.e.of.pf.loglik.of.mif.estimate,
+       file = paste0(name_str,".RData"))
+}
+
---
+++ Single-species/Lum/SRJF/model/all_shared_noxiJlambda.R
@@ -0,0 +1,337 @@
+library(reshape2)
+library(magrittr)
+library(foreach)
+library(readxl)
+library(doParallel)
+registerDoParallel(cores=36)
+library(pomp)
+library(panelPomp)
+library(tidyverse)
+
+# Mesocosm_data = read_excel("~/Desktop/Research/D_P/Mesocosmdata.xlsx",2)
+Mesocosm_data = read_excel("/home/ybb/D_P/Mesocosmdata.xlsx",2)
+
+sed = 0923
+set.seed(0923)
+
+name_str = "all_shared_no_xiJ"
+run_level <- 2
+
+dentNoPara <- Mesocosm_data[1:100, ]
+dentNoPara <- subset(dentNoPara, select = c(rep, day, lum.adult))
+dentNoPara <- dentNoPara[100: 1, ]
+#Convert sampling date to natural date. Samples are collected every 5 days after the #first 7 days.
+dentNoPara$day = (dentNoPara$day - 1) * 5 + 7
+data = list()
+trails = c("A","B","C","D","E","F","G","H","I","J")
+for (i in 1: length(trails)){
+  data[[i]]=subset(dentNoPara, select = c("day", "lum.adult"),
+                   dentNoPara$rep == trails[i])
+}
+
+dyn_rpro <- Csnippet("
+                      double Si_term, F_term , Ji_term;
+                      double noiSi  ,noiF,noiJi;
+                      double delta = 0.013; //fraction of volume replaced day-1
+
+                      noiSi = rnorm(0, sigSi * sqrt(dt));
+                      noiJi = rnorm(0, sigJi * sqrt(dt));
+                      noiF = rnorm(0, sigF * sqrt(dt));
+
+
+                      //------------Si-------------
+                      Si_term = 0.1 * Ji * dt - theta_Si * Si * dt -  delta * Si * dt + Si * noiSi;
+                        
+                      //-----------Ji-------------
+                      
+                      Ji_term = ri * f_Si * F * Si  * dt  -  0.1 * Ji * dt - theta_Ji * Ji * dt - delta * Ji * dt + Ji * noiJi;
+                      
+                      //-----------F---------------
+
+                      F_term =  F * noiF - f_Si * F * (Si + 1 * Ji) * dt  - delta * F * dt + 0.37 * dt;
+
+
+                      //T_D and T_I are the S and I Daphnia sample out
+                      F += F_term;
+                      Si += Si_term;
+                      Ji += Ji_term;
+                  
+                      
+                      
+                      if (Si < 0.0 || Si > 1e5) {
+                        Si = 0.0;
+                        error_count += 1;
+                      }
+                      if (F < 0.0 || F > 1e20) {
+                        F = 0.0;
+                        error_count += 1000;
+                      }
+                      if (Ji < 0.0 || Ji > 1e5) {
+                        Ji = 0.0;
+                        error_count += 0.001;
+                      }
+  
+                      
+                      T_Si = fabs(Si);
+                      
+
+                      ")
+
+# Initial state. Assume t0 = day 4
+dyn_init = Csnippet("
+                     Si = 3; 
+                     F = 16.667;
+                     Ji = 0;
+                     
+                     T_Si = 0.0;
+                     
+                     error_count = 0.0;
+                     
+                     ")
+
+
+
+dmeas = Csnippet("
+                 
+                 if (error_count > 0.0) {
+                   lik = -150;
+                  }
+                 else{
+                    if(give_log){
+                    lik  = dnbinom_mu(lumadult,k_Si,T_Si,give_log);
+                    }
+                    else{
+                    lik  = dnbinom_mu(lumadult,k_Si,T_Si,give_log);
+                    }
+                      }
+                 ")
+
+rmeas = Csnippet("
+                 //double delta = 0.013;
+                 lumadult = rnbinom_mu(k_Si,T_Si);
+                 ")
+
+pt <- parameter_trans(
+  log = c( "sigSi","sigF","f_Si",
+           "ri","k_Si","sigJi",
+           "theta_Si","theta_Ji")
+)
+
+pomplist=list()
+
+parameters=  c( "sigSi" = 0, "sigF"= 0.1,"f_Si"= 0.1,
+                "ri"= 10,"k_Si"= 0.1,"sigJi"= 0.1,
+                "theta_Si"= 0.1,"theta_Ji"= 0.1)
+
+
+for (i in 1:10){
+  colnames(data[[i]]) <- c('day', 'lumadult')
+  pomp(data = data[[i]],
+       times = "day",
+       t0=1,
+       rprocess=euler(dyn_rpro,delta.t=1/4),
+       rinit = dyn_init,
+       dmeasure = dmeas,
+       rmeasure = rmeas,
+       partrans = pt,
+       obsnames = c("lumadult"),
+       accumvars = c("error_count"),
+       paramnames = c( "sigSi","sigF","f_Si",
+                       "ri","k_Si","sigJi",
+                       "theta_Si","theta_Ji"),
+       statenames = c("Si","Ji" ,"error_count", "F", "T_Si")
+  ) -> pomplist[[i]]
+  coef(pomplist[[i]])=parameters
+}
+names(pomplist)=paste("u", 1:10,sep = "")
+
+
+
+shared_parameter = c(
+  ri = 708.6439, f_Si = 0.0002316239, theta_Si = 0.5255919, theta_Ji = 0.07714973,
+  sigSi = 0, sigJi = 0.3588666, sigF = 0.0458815, k_Si = 2.288263
+)
+
+panelfood = panelPomp(pomplist, shared=shared_parameter)
+
+
+algorithmic.params <- list(
+  Np =     c(50, 500, 1e4),
+  Np_rep = c( 2,  10,  10),
+  Mp =     c(50, 500, 1e4),
+  Nmif =   c( 2,  320, 250)
+)
+
+
+dent_rw.sd= 0.05
+parameter_candidates = list(shared_parameter)
+names(parameter_candidates)=c("shared")
+U = length(panelfood)
+{
+  foreach(
+    i = 1:(10*getDoParWorkers()),
+    .packages = c("pomp", "panelPomp"),
+    .inorder = FALSE,
+    .options.multicore = list(set.seed = TRUE)
+  ) %dopar%
+    {
+      guessed.parameter.values <- parameter_candidates
+      mif2(
+        panelfood,
+        Nmif = 200,
+        # shared.start = shared_start,
+        shared.start = guessed.parameter.values$shared,
+        # specific.start = unit_start,
+        rw.sd = rw_sd(
+          sigSi=0,
+          sigF=dent_rw.sd,
+          theta_Si=dent_rw.sd,
+          k_Si = dent_rw.sd,
+          f_Si=dent_rw.sd,
+          ri=dent_rw.sd,
+          sigJi = dent_rw.sd,
+          theta_Ji = dent_rw.sd),
+        cooling.type = "geometric",
+        cooling.fraction.50 = 0.7,
+        Np = algorithmic.params$Mp[run_level]
+      ) -> m1
+      
+      ll <- replicate(n = algorithmic.params$Np_rep[run_level],
+                      unitlogLik(pfilter(m1,
+                                         Np = algorithmic.params$Np[run_level])))
+      
+      list(mif = m1,
+           ll = panel_logmeanexp(x = ll,
+                                 MARGIN = 1,
+                                 se = TRUE))
+    }
+} -> mf1
+
+log_list = c()
+for ( i in 1:length(mf1)){
+  log_list = c(log_list,mf1[[i]]$ll[1])
+}
+select = order(log_list,decreasing = TRUE)[1:ceiling(length(mf1)/4)]
+shared_dataframe = data.frame(t(mf1[[select[1]]]$mif@shared))
+for (i in 1:length(select)){
+  shared_dataframe[i,] = t(mf1[[select[i]]]$mif@shared)
+}
+shared_dataframe <- shared_dataframe[rep(1:nrow(shared_dataframe), each = 4), ]
+dent_rw.sd = 0.04
+#round 2
+{
+  foreach(
+    i = 1:(10*getDoParWorkers()),
+    .packages = c("pomp", "panelPomp"),
+    .inorder = FALSE,
+    .options.multicore = list(set.seed = TRUE)
+  ) %dopar%
+    {
+      share_para_temp = as.numeric(shared_dataframe[i,])
+      names(share_para_temp) = colnames(shared_dataframe)
+      
+      mif2(
+        panelfood,
+        Nmif = 200,
+        shared.start = share_para_temp,
+        rw.sd = rw_sd(
+          sigSi=0,
+          sigF=dent_rw.sd,
+          theta_Si=dent_rw.sd,
+          k_Si = dent_rw.sd,
+          f_Si=dent_rw.sd,
+          ri=dent_rw.sd,
+          sigJi = dent_rw.sd,
+          theta_Ji = dent_rw.sd),
+        cooling.type = "geometric",
+        cooling.fraction.50 = 0.7,
+        Np = algorithmic.params$Mp[run_level]
+      ) -> m1
+      
+      ll <- replicate(n = algorithmic.params$Np_rep[run_level],
+                      unitlogLik(pfilter(m1,
+                                         Np = algorithmic.params$Np[run_level])))
+      
+      list(mif = m1,
+           ll = panel_logmeanexp(x = ll,
+                                 MARGIN = 1,
+                                 se = TRUE))
+    }
+} -> mf2
+
+
+log_list = c()
+for ( i in 1:length(mf2)){
+  log_list = c(log_list,mf2[[i]]$ll[1])
+}
+select = order(log_list,decreasing = TRUE)[1:ceiling(length(mf2)/4)]
+shared_dataframe = data.frame(t(mf2[[select[1]]]$mif@shared))
+for (i in 1:length(select)){
+  shared_dataframe[i,] = t(mf2[[select[i]]]$mif@shared)
+}
+shared_dataframe <- shared_dataframe[rep(1:nrow(shared_dataframe), each = 4), ]
+dent_rw.sd = 0.03
+
+#round 3
+{
+  foreach(
+    i = 1:(10*getDoParWorkers()),
+    .packages = c("pomp", "panelPomp"),
+    .inorder = FALSE,
+    .options.multicore = list(set.seed = TRUE)
+  ) %dopar%
+    {
+      
+      share_para_temp = as.numeric(shared_dataframe[i,])
+      names(share_para_temp) = colnames(shared_dataframe)
+      
+      mif2(
+        panelfood,
+        Nmif = 400,
+        shared.start = share_para_temp,
+        rw.sd = rw_sd(
+          sigSi=0,
+          sigF=dent_rw.sd,
+          theta_Si=dent_rw.sd,
+          k_Si = dent_rw.sd,
+          f_Si=dent_rw.sd,
+          ri=dent_rw.sd,
+          sigJi = dent_rw.sd,
+          theta_Ji = dent_rw.sd),
+        cooling.type = "geometric",
+        cooling.fraction.50 = 0.7,
+        Np = algorithmic.params$Mp[run_level]
+      ) -> m1
+      
+      ll <- replicate(n = algorithmic.params$Np_rep[run_level],
+                      unitlogLik(pfilter(m1,
+                                         Np = algorithmic.params$Np[run_level])))
+      
+      list(mif = m1,
+           ll = panel_logmeanexp(x = ll,
+                                 MARGIN = 1,
+                                 se = TRUE))
+    }
+} -> mf
+
+
+lls <- matrix(unlist(sapply(mf, getElement, "ll")), nrow = 2)
+best <- which.max(lls[1,])
+mif.estimate <- coef(mf[[best]]$mif)
+pf.loglik.of.mif.estimate <- unname(mf[[best]]$ll[1])
+s.e.of.pf.loglik.of.mif.estimate <- unname(mf[[best]]$ll[2])
+
+pf.loglik.of.mif.estimate
+s.e.of.pf.loglik.of.mif.estimate
+
+if (run_level == 2){
+  save(mf,mf1,mf2,lls,best,mif.estimate,pf.loglik.of.mif.estimate,
+       s.e.of.pf.loglik.of.mif.estimate,
+       file = paste0(name_str,".RData"))
+}
+
+if (run_level == 3){
+  save(lls,best,mif.estimate,pf.loglik.of.mif.estimate,
+       s.e.of.pf.loglik.of.mif.estimate,
+       file = paste0(name_str,".RData"))
+}
